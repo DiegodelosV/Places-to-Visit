fb6edc8cba7f871c4c340692cb6bff09bbf34fa6
DiegodelosV <diegodelosv@gmail.com>
2024-09-09 11:03:06 -0300

    creación binding de formularios y llamado API restcountries

--- src/components/PlaceForm.vue
+++ src/components/PlaceForm.vue
@@ -1,9 +1,37 @@
 <template>
-    <div>
-
-    </div>
+  <div>
+    <label for="place-select">Selecciona un lugar</label>
+    <select v-model="selectPlace" id="place-select">
+      <option v-for="place in places" :key="place.name">
+        {{ place.name }}
+      </option>
+    </select>
+    <button @click="addPlace">Añadir un lugar</button>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      places: [],
+      selectPlace: null,
+    };
+  },
+  methods: {
+    fetchPlaces() {
+      axios.get("https://restcountries.com/v3.1/all").then((response) => {
+        this.places = response.data;
+      });
+    },
 
+    addPlace() {
+      this.$emit("add-place", this.selectPlace);
+    },
+  },
+  mounted() {
+    this.fetchPlaces();
+  },
+};
 </script>
